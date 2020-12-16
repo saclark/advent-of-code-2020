b0c2160b2fa273c2a46cdaff32cd860822df1dc3
Scott Clark <scottclark11@gmail.com>
2020-12-15 20:44:30 -0500

    completes day 15

---
+++ day15/part1.rkt
@@ -0,0 +1,38 @@
+#lang racket/base
+
+(require racket/file
+         racket/list
+         racket/match
+         racket/string)
+
+(define (solve starting-numbers n)
+  (play-game (make-history starting-numbers)
+             (vector-ref starting-numbers (- (vector-length starting-numbers) 1))
+             (+ 1 (vector-length starting-numbers))
+             n))
+
+(define (make-history starting-numbers)
+  (for/fold ([history (hash)])
+            ([i (in-range (vector-length starting-numbers))])
+    (hash-set history (vector-ref starting-numbers i) (list (+ i 1)))))
+
+(define (play-game game-history prev-num starting-turn ending-turn)
+  (for/fold ([history game-history]
+             [p prev-num]
+             #:result p)
+            ([i (range starting-turn (+ 1 ending-turn))])
+      (let*-values ([(n n-history) (next history i p)])
+        (values (hash-set history n n-history) n))))
+
+(define (next history i p)
+  (let ([n (match (hash-ref history p)
+             [(list pi) 0]
+             [(list pi ppi) (- pi ppi)])])
+    (match (hash-ref history n '())
+      ['() (values n (list i))]
+      [(list pi _ ...) (values n (list i pi))])))
+
+(define (parse-input input-file-path)
+  (list->vector (map string->number (string-split (file->string input-file-path) ","))))
+
+(solve (parse-input "input.txt") 2020)
---
+++ day15/part2.rkt
@@ -0,0 +1,20 @@
+#lang racket/base
+
+(require racket/file
+         racket/list
+         racket/string)
+
+(define (solve starting-numbers n)
+  (define history (make-hasheq))
+  (for ([(n i) (in-indexed (drop-right starting-numbers 1))])
+    (hash-set! history n (add1 i)))
+  (for/fold ([p (last starting-numbers)])
+            ([i (in-range (add1 (length starting-numbers)) (add1 n))])
+    (let ([last-seen (hash-ref history p #f)])
+      (hash-set! history p (sub1 i))
+      (if last-seen (- (sub1 i) last-seen) 0))))
+
+(define (parse-input input-file-path)
+  (map string->number (string-split (file->string input-file-path) ",")))
+
+(solve (parse-input "input.txt") 30000000)
