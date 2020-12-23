f794d578d21d567848eafda29da7ace2ed13940c
Scott Clark <scottclark11@gmail.com>
2020-12-22 20:10:32 -0500

    adds types to day 1 part 1 and day 15 part 2

--- day01/part1.rkt
+++ day01/part1.rkt
@@ -1,22 +1,30 @@
-#lang racket/base
+#lang typed/racket/base
 
 (require racket/file
          racket/match
-         rackunit)
+         typed/rackunit)
 
+(: solve (-> (Listof Integer) (U Integer #f)))
 (define (solve entries)
+  (: solve-aux (-> Integer (Listof Integer) (U Integer #f)))
+  (define (solve-aux x xs)
+    (match xs
+      [(list y rest ...) (if (= 2020 (+ x y)) (* x y) (solve-aux x rest))]
+      [else #f]))
   (match entries
-    [(list x xs ...)
-     (let ([result (solve* x xs)])
-       (if result result (solve xs)))]
+    [(list x xs ...) (let ([result (solve-aux x xs)])
+                       (if result result (solve xs)))]
     [else #f]))
 
-(define (solve* x xs)
-  (match xs
-    [(list y rest ...)
-     (if (= 2020 (+ x y))
-         (* x y)
-         (solve* x rest))]
-    [else #f]))
+(: parse-input (-> String (Listof Integer)))
+(define (parse-input input-file-path)
+  (map string->integer (file->lines input-file-path)))
 
-(check-eqv? (solve (map string->number (file->lines "input.txt"))) 471019)
+(: string->integer (-> String Integer))
+(define (string->integer s)
+  (let ([n (string->number s)])
+    (cond
+      [(exact-integer? n) n]
+      [else (raise-argument-error 'string->integer "a string convertible to an exact-integer" s)])))
+
+(check-eqv? (solve (parse-input "input.txt")) 471019)
--- day15/part2.rkt
+++ day15/part2.rkt
@@ -1,21 +1,31 @@
-#lang racket/base
+#lang typed/racket/base
 
 (require racket/file
          racket/list
          racket/string
-         rackunit)
+         typed/rackunit)
 
-(define (solve starting-numbers n)
+(: solve (-> (Listof Integer) Integer Integer))
+(define (solve starting-numbers num)
+  (: history (HashTable Integer Integer))
   (define history (make-hasheq))
-  (for ([(n i) (in-indexed (drop-right starting-numbers 1))])
-    (hash-set! history n (add1 i)))
-  (for/fold ([p (last starting-numbers)])
-            ([i (in-range (add1 (length starting-numbers)) (add1 n))])
-    (let ([last-seen (hash-ref history p #f)])
-      (hash-set! history p (sub1 i))
-      (if last-seen (- (sub1 i) last-seen) 0))))
+  (for ([(num turn) (in-indexed (drop-right starting-numbers 1))])
+    (hash-set! history num (add1 turn)))
+  (for/fold ([prev (last starting-numbers)])
+            ([turn (in-range (add1 (length starting-numbers)) (add1 num))])
+    (let ([last-seen (hash-ref history prev #f)])
+      (hash-set! history prev (sub1 turn))
+      (if last-seen (- (sub1 turn) last-seen) 0))))
 
+(: parse-input (-> String (Listof Integer)))
 (define (parse-input input-file-path)
-  (map string->number (string-split (file->string input-file-path) ",")))
+  (map string->integer (string-split (file->string input-file-path) ",")))
+
+(: string->integer (-> String Integer))
+(define (string->integer s)
+  (let ([n (string->number s)])
+    (cond
+      [(exact-integer? n) n]
+      [else (raise-argument-error 'string->integer "a string convertible to an exact-integer" s)])))
 
 (check-eqv? (solve (parse-input "input.txt") 30000000) 1437692)
