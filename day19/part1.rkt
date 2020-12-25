346261cbea8542a97f0bc7f4f2251dbf638ccb42
Scott Clark <scottclark11@gmail.com>
2020-12-24 20:33:49 -0500

    WIP still doesn't quite typecheck

--- day19/part1.rkt
+++ day19/part1.rkt
@@ -1,4 +1,4 @@
-#lang racket/base
+#lang typed/racket/base
 
 (require racket/file
          racket/function
@@ -7,14 +7,23 @@
          racket/string
          typed/rackunit)
 
-(define (solve rules messages)
-  (count identity (map (curry matches-rule? rules 0) messages)))
+(define-type Rule (Listof Rule-Option))
+(define-type Rule-Option (Listof (U Integer Char)))
+(define-type Message (Listof Char))
 
+(: solve (-> (Vectorof Rule) (Listof String) Integer))
+(define (solve rules messages)
+  (: matches-rule-zero? (-> String Boolean))
+  (define matches-rule-zero? (λ (msg) (matches-rule? rules 0 msg)))
+  (count identity (map matches-rule-zero? messages)))
+
+(: matches-rule? (-> (Vectorof Rule) Integer String Boolean))
 (define (matches-rule? rules rule-number message)
   (match-let ([(cons partial-match? unmatched-chars)
                (partially-matches-rule? rules rule-number (string->list message))])
     (and partial-match? (null? unmatched-chars))))
 
+(: partially-matches-rule? (-> (Vectorof Rule) (U Integer Char) Message (Pairof Boolean Message)))  
 (define (partially-matches-rule? rules v message)
   (cond
     [(null? message)
@@ -25,8 +34,8 @@
           (cons #f message))]
     [(exact-integer? v)
       (let* ([rule (vector-ref rules v)])
-        (or (for/or ([option (in-list rule)])
-              (for/fold ([result (cons #t message)]
+        (or (for/or : (U (Pairof Boolean Message) False) ([option (in-list rule)])
+              (for/fold ([result : (Pairof Boolean Message) (cons #t message)]
                          #:result (if (car result) result #f))
                         ([v (in-list option)])
                 (if (car result)
@@ -34,28 +43,44 @@
                     (cons #f message))))
             (cons #f message)))]))
 
+(: parse-input (-> String (Values (Vectorof Rule) (Listof String))))
 (define (parse-input input-file-path)
   (match-let ([(list rules-str messges-str) (string-split (file->string input-file-path) "\n\n")])
     (values (parse-rules rules-str) (string-split messges-str "\n"))))
 
+(: parse-rules (-> String (Vectorof Rule)))
 (define (parse-rules rules-str)
+  (: mapcar (All (A B) (-> (Listof (Pairof A B)) (Listof A))))
+  (define mapcar (λ (pairs) (map car pairs)))
+
+  (: make-rule-tree (-> (Listof (Pairof Integer Rule)) (Vectorof Rule)))
+  (define make-rule-tree
+    (λ (rules)
+      (let* ([len : Integer (add1 (apply max (map car rules)))]
+             [vec : (Vectorof Rule) (make-vector len)])
+        vec)))
+  
   (let* ([rules (map parse-rule-line (string-split rules-str "\n"))]
-         [rule-tree (make-vector (add1 (apply max (map car rules))))])
+         [rule-tree (make-rule-tree rules)])
     (for ([rule (in-list rules)])
       (vector-set! rule-tree (car rule) (cdr rule)))
     rule-tree))
 
+(: parse-rule-line (-> String (Pairof Integer Rule)))
 (define (parse-rule-line line)
   (match-let* ([(list id spec) (string-split line ": ")])
     (cons (string->integer id) (parse-rule-spec spec))))
 
+(: parse-rule-spec (-> String Rule))
 (define (parse-rule-spec spec)
+  (: parse-rule-option (-> String (Listof Integer)))
   (define parse-rule-option (λ (s) (map string->integer (string-split s " "))))
   (match (string-split spec " | ")
     [(list "\"a\"") (list (list #\a))]
     [(list "\"b\"") (list (list #\b))]
     [(list rule-options ...) (map parse-rule-option rule-options)]))
 
+(: string->integer (-> String Integer))
 (define (string->integer s)
   (let ([n (string->number s)])
     (cond
