4820397c76dd6ffc9e54faa4d056f4f7291fdd92
Scott Clark <scottclark11@gmail.com>
2021-01-03 20:38:26 -0500

    implements day 17, part 2 in typed racket

--- day17/part2.rkt
+++ day17/part2.rkt
@@ -1,114 +1,138 @@
-#lang racket/base
+#lang typed/racket/base
 
 (require racket/file
          racket/function
          racket/list
          racket/match
-         rackunit
+         typed/rackunit
          threading)
 
+(: solve (-> (Tree Integer) Natural Natural))
 (define (solve space cycles)
   (tree-leaf-count
-    (for/fold ([s space])
+    (for/fold ([s : (Tree Integer) space])
               ([_ (in-range cycles)])
       (run-iteration s))))
 
+(: run-iteration (-> (Tree Integer) (Tree Integer)))
 (define (run-iteration space)
-  (define new-space (make-hasheq))
+  (define new-space : (Tree Integer) (make-hasheq))
   (for ([coord (in-list (all-space-coords space))])
     (when (make-active? space coord) (tree-add! new-space coord)))
   new-space)
 
+(: make-active? (-> (Tree Integer) (Listof Integer) Boolean))
 (define (make-active? space coord)
   (let ([n (active-neighbor-count space coord)])
     (if (tree-has-branch? space coord)
       (if (or (= n 2) (= n 3)) #t #f)
       (if (not (= n 3)) #f #t))))
 
+(: active-neighbor-count (-> (Tree Integer) (Listof Integer) Integer))
 (define (active-neighbor-count space coord)
-  (length (filter (curry tree-has-branch? space) (neighboring-coords coord))))
+  (length (filter (curry (ann tree-has-branch? (-> (Tree Integer) (Listof Integer) Boolean)) space)
+                  (neighboring-coords coord))))
 
+(: all-space-coords (-> (Tree Integer) (Listof (Listof Integer))))
 (define (all-space-coords space)
-  (define coords (make-hasheq))
+  (define coords : (Tree Integer) (make-hasheq))
   (for ([coord (in-list (tree-branches space))])
     (tree-add! coords coord)
     (for ([neighbor (neighboring-coords coord)])
       (tree-add! coords neighbor)))
   (tree-branches coords))
 
+(: neighboring-coords (-> (Listof Integer) (Listof (Listof Integer))))
 (define (neighboring-coords coord)
   (let ([offsets (permutations (length coord) '(-1 0 1))])
-    (for/fold ([acc '()])
+    (for/fold ([acc : (Listof (Listof Integer)) '()])
               ([offset (in-list offsets)])
-      (if (for/and ([o (in-list offset)]) (= 0 o))
+      (if (ann (for/and ([o (in-list offset)]) (= 0 o)) Boolean)
           acc
           (cons (shift-coord coord offset) acc)))))
 
+(: permutations (All (A) (-> Integer (Listof A) (Listof (Listof A)))))
 (define (permutations size elements)
   (if (zero? size)
       '(())
-      (flatmap (λ (p) (map (λ (e) (cons e p)) elements))
+      (flat-map (λ ([permutation : (Listof A)]) (map (λ ([elem : A]) (cons elem permutation)) elements))
                (permutations (sub1 size) elements))))
 
-(define (flatmap proc ls)
-  (foldl (λ (l acc) (append acc l)) '() (map proc ls)))
+(: flat-map (All (A B) (-> (-> A (Listof B)) (Listof A) (Listof B))))
+(define (flat-map proc ls)
+  (for/fold ([acc : (Listof B) '()])
+            ([l (in-list (map proc ls))])
+    (append acc l)))
 
+(: shift-coord (-> (Listof Integer) (Listof Integer) (Listof Integer)))
 (define (shift-coord coord offsets)
-  (for/fold ([acc '()]
+  (for/fold ([acc : (Listof Integer) '()]
              #:result (reverse acc))
             ([v (in-list coord)]
              [offset (in-list offsets)])
     (cons (+ v offset) acc)))
 
+(define-type (Tree A) (Rec T (HashTable A T)))
+
+(: tree-leaf-count (All (A) (-> (Tree A) Natural)))
 (define (tree-leaf-count tree)
-  (for/fold ([count 0])
-            ([(k v) (in-hash tree)])
+  (for/fold ([count : Natural 0])
+            ([([k : A] [v : (Tree A)]) (in-hash tree)])
     (let ([sub-count (tree-leaf-count v)])
       (if (zero? sub-count)
           (+ count 1)
           (+ count sub-count)))))
 
+(: tree-branches (All (A) (-> (Tree A) (Listof (Listof A)))))
 (define (tree-branches tree)
-  (for/fold ([branches '()])
-            ([(k v) (in-hash tree)])
+  (for/fold ([branches : (Listof (Listof A)) '()])
+            ([([k : A] [v : (Tree A)]) (in-hash tree)])
     (let* ([sub-branches (tree-branches v)]
            [branch (if (null? sub-branches)
                        (list (list k))
-                       (map (curry cons k) (tree-branches v)))])
+                       (map (curry (ann cons (-> A (Listof A) (Listof A))) k) (tree-branches v)))])
       (append branches branch))))
 
+(: tree-has-branch? (All (A) (-> (Tree A) (Listof A) Boolean)))
 (define (tree-has-branch? tree nodes)
   (if (null? nodes)
       #t
-      (let ([t (hash-ref tree (car nodes) #f)])
-        (if t (tree-has-branch? t (cdr nodes)) #f))))
+      (let ([sub-tree : (U (Tree A) False) (hash-ref tree (car nodes) #f)])
+        (if sub-tree (tree-has-branch? sub-tree (cdr nodes)) #f))))
 
+(: tree-add! (All (A) (-> (Tree A) (Listof A) Void)))
 (define (tree-add! tree nodes)
   (unless (null? nodes)
     (let* ([node (car nodes)]
-           [sub-tree (hash-ref tree node #f)])
+           [sub-tree : (U (Tree A) False) (hash-ref tree node #f)])
       (if sub-tree
           (tree-add! sub-tree (cdr nodes))
-          (let ([sub-tree (make-hasheq)])
+          (let ([sub-tree : (Tree A) (make-hasheq)])
             (tree-add! sub-tree (cdr nodes))
             (hash-set! tree node sub-tree))))))
 
+(: parse-input (-> Integer String (Tree Integer)))
 (define (parse-input dimensions input-file-path)
-  (define chars->states (λ (chars) (map char->state chars)))
+  (: char->state (-> Char Boolean))
   (define char->state (λ (char) (match char [#\. #f] [#\# #t])))
+  (: chars->states (-> (Listof Char) (Listof Boolean)))
+  (define chars->states (λ (chars) (map char->state chars)))
   (~> input-file-path
       file->lines
       (map string->list _)
       (map chars->states _)
       (layer->space dimensions _)))
 
+(: layer->space (-> Integer (Listof (Listof Boolean)) (Tree Integer)))
 (define (layer->space dimensions layer)
   (let* ([height (sub1 (length layer))]
-         [width (sub1 (apply max (map length layer)))]
-         [pocket-space (make-hasheq)])
+         [width (sub1 (apply max (map (ann length (-> (Listof Boolean) Index)) layer)))]
+         [pocket-space : (Tree Integer) (make-hasheq)])
     (for ([(row y) (in-indexed layer)])
       (for ([(active? x) (in-indexed row)])
-        (when active? (tree-add! pocket-space (append (list x y) (make-list (- dimensions 2) 0))))))
+        (when active?
+          (tree-add! pocket-space
+                     (append (list x y) (make-list (- dimensions 2) 0))))))
      pocket-space))
 
 ;;(check-eqv? (solve (parse-input 3 "input.txt") 6) 362)
